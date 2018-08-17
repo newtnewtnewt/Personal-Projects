51c83511a0bf41cf4e6bffab7a1c91917eaf8f60
newtnewtnewt <42472588+newtnewtnewt@users.noreply.github.com>
2018-08-17 11:38:21 -0400

    NewtLike Upload

---
+++ Newtlike/src/LevelCreation/LevelTest.java
@@ -0,0 +1,12 @@
+package LevelCreation;
+
+import java.io.FileNotFoundException;
+
+public class LevelTest {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		LevelAssembly x = new LevelAssembly(1);
+		x.levelDisplay();
+	}
+
+}
