08d08b49f3bea5000affdde42a6e17d4c760abbb
newtnewtnewt <42472588+newtnewtnewt@users.noreply.github.com>
2019-02-19 11:47:42 -0500

    Uploaded CSE 385

---
+++ CSE 385/Lecture/Day 2/dbo.tblFriends.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[tblFriends] (
+    [friendId] INT          IDENTITY (1, 1) NOT NULL,
+    [fName]    VARCHAR (20) DEFAULT ('') NOT NULL,
+    [lName]    VARCHAR (20) DEFAULT ('') NOT NULL,
+    [age]      INT          DEFAULT ((0)) NOT NULL,
+    [phoneNum] VARCHAR (20) DEFAULT ('none') NOT NULL,
+    PRIMARY KEY CLUSTERED ([friendId] ASC)
+);
+
