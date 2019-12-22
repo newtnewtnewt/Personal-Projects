2549bcd0cdae908a998e692e698c5cb23169f90d
newtnewtnewt <42472588+newtnewtnewt@users.noreply.github.com>
2019-12-21 21:01:29 -0500

    Deleting time

--- CSE287 - Computer Graphics/Lab/Lab01/CSE287Lab1/CSE287Lab1/CSE287Raytrace/MySquare.cpp
+++
@@ -1,5 +0,0 @@
-#include "MySquare.h"
-	
-MySquare::MySquare(double sideLength, color c, int xVal, int yVal) : 
-	sideLength(sideLength), c(c), xVal(xVal), yVal(yVal), MyRectangle(xVal, yVal, c, sideLength, sideLength){ }
-MySquare::~MySquare() { }
